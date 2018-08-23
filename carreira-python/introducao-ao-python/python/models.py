f837bb83a9942025a21ac961847a2f343dfce1e9
CodexZombie <walvesdg@gmail.com>
2018-08-23 12:48:51 -0300

    Class e OO

--- carreira-python/introducao-ao-python/python/models.py
+++ carreira-python/introducao-ao-python/python/models.py
@@ -1,6 +1,33 @@
 # -*- coding: UTF-8 -*-
 # python/models.py
 
-class Perfil():
+class Perfil(object):
     'Classe padrão para perfis de usuários'
-    
+    def __init__(self, nome, telefone, empresa):
+        self.nome = nome
+        self.telefone = telefone
+        self.empresa = empresa
+
+    def imprime(self):
+        print 'Nome: %s' % (self.nome)
+        print 'Telefone: %s' % (self.telefone)
+        print 'Empresa: %s' % (self.empresa)
+
+class Data(object):
+    def __init__ (self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def imprime(self):
+        print '%s/%s/%s' % (str(self.dia), str(self.mes), str(self.ano))
+
+class Pessoa(object):
+    def __init__(self, nome, peso, altura):
+        self.nome = nome
+        self.peso = peso
+        self.altura = altura
+
+    def imprime(self):
+        imc = self.peso / (self.altura*self.altura)
+        print 'IMC de %s: %s' % (self.nome, str(imc))
