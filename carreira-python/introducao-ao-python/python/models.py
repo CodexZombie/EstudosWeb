4e26d08471f3ded5bc807851e5e0a9034c849857
CodexZombie <walvesdg@gmail.com>
2018-08-25 19:24:09 -0300

    handling exceptions

--- carreira-python/introducao-ao-python/python/models.py
+++ carreira-python/introducao-ao-python/python/models.py
@@ -29,20 +29,32 @@ class Perfil(object):
         arquivo = open(nome_arquivo, 'r')
         for linha in arquivo:
             valores = linha.split(',')
+            if(len(valores) is not 3):
+                raise Perfil_Error('Uma linha no arquivo deve ter 3 valores')
             perfis.append(Perfil(*valores))
         arquivo.close()
         return perfis
 
+class Perfil_Error(Exception):
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
+
+    def __str__(self):
+        return repr(self.mensagem)
 
 
 class Perfil_Vip(Perfil):
     'Classe padrão para perfis de usuários VIPs'
-    def __init__(self, nome, telefone, empresa, apelido):
+    def __init__(self, nome, telefone, empresa, apelido=''):
         super(Perfil_Vip, self).__init__(nome, telefone, empresa)
         self.apelido = apelido
 
     def obter_creditos(self):
         return super(Perfil_Vip, self).obter_curtidas() * 10.0
+
+
+
+
 
 
 
