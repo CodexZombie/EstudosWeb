caf766cb7220cdeb4021cb946630e9d585c341b6
CodexZombie <walvesdg@gmail.com>
2018-09-10 19:26:55 -0300

    @staticmethod

--- carreira-python/python3/oo/conta.py
+++ carreira-python/python3/oo/conta.py
@@ -3,39 +3,66 @@
 
 class Conta:
 
+
     def __init__(self, numero, titular, saldo, limite):
-        print('Construindo objeto... []'.format(self))
+        print('Construindo objeto...\n{}'.format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
+
     def extrato(self):
         print('O saldo da conta do {} é R${}'.format(self.__titular, self.__saldo))
+
 
     def deposita(self, valor):
         self.__saldo += valor
 
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
+
     @property
     def saldo(self):
         return self.__saldo
 
+
     @property
-    def titular(self):
+    def get_titular(self):
         return self.__titular
+
 
     @property
     def limite(self):
         return self.__limite
 
+
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
 
