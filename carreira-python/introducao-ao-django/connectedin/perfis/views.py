a2703342fe18a1fa08fa5a66b1ce5ed17d21cc44
CodexZombie <walvesdg@gmail.com>
2018-09-11 23:09:16 -0300

    template tags {% %}

--- carreira-python/introducao-ao-django/connectedin/perfis/views.py
+++ carreira-python/introducao-ao-django/connectedin/perfis/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
 
 
 def exibir(request, perfil_id):
