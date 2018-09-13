fa844be75073edb8a6f270c90056f144bdb59d32
CodexZombie <walvesdg@gmail.com>
2018-09-12 21:55:24 -0300

    adicionando bootstrap

--- carreira-python/introducao-ao-django/connectedin/perfis/views.py
+++ carreira-python/introducao-ao-django/connectedin/perfis/views.py
@@ -1,7 +1,8 @@
 # ../connectedin/
 # ../perfis/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from perfis.models import Perfil
 
 def index(request):
@@ -9,7 +10,16 @@ def index(request):
 
 
 def exibir(request, perfil_id):
-
     perfil = Perfil.objects.get(id=perfil_id)
-
     return render(request, 'perfil.html', { "perfil" : perfil })
+
+
+def convidar(request, perfil_id):
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return redirect('index')
+
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=6)
