c6470ddf0b62a91922c937f390177abf722b79e8
CodexZombie <walvesdg@gmail.com>
2018-09-21 21:46:39 -0300

    template method

--- carreira-python/design-patterns-python/calculador_de_impostos.py
+++ carreira-python/design-patterns-python/calculador_de_impostos.py
@@ -5,15 +5,29 @@ class Calculador_de_impostos(object):
     def realiza_calculo(self, orcamento, imposto):
         valor = imposto.calcula(orcamento)
         print valor
-        
+
 
 if __name__ == '__main__':
 
-    from orcamento import Orcamento
-    from impostos import ICMS, ISS
+    from orcamento import Orcamento, Item
+    from impostos import ICMS, ISS, IKCV, ICPP
 
-    orcamento = Orcamento(500.0)
+    orcamento = Orcamento()
+
+    orcamento.adiciona_item(Item('Item 1', 20))
+    orcamento.adiciona_item(Item('Item 2', 20))
+    orcamento.adiciona_item(Item('Item 3', 20))
+    orcamento.adiciona_item(Item('Item 4', 20))
+    orcamento.adiciona_item(Item('Item 5', 200))
+
     calculador_de_impostos = Calculador_de_impostos()
-    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
-    calculador_de_impostos.realiza_calculo(orcamento, ISS())
     
+    print 'ICMS R$'
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
+    print 'ISS R$'
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    print 'ICPP R$'
+    calculador_de_impostos.realiza_calculo(orcamento, ICPP())
+    print 'IKCV R$'
+    calculador_de_impostos.realiza_calculo(orcamento, IKCV())
+
