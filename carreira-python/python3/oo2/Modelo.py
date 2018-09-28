a69a09994acdfb6214dd8526e2e87b8dfc2da876
CodexZombie <walvesdg@gmail.com>
2018-09-28 08:10:22 -0300

    herança

--- carreira-python/python3/oo2/Modelo.py
+++ carreira-python/python3/oo2/Modelo.py
@@ -1,50 +1,39 @@
 # -*- coding: UTF-8 -*-
 # ../python3/oo2/Modelo.py
 
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    def dar_like(self):
+        self._likes += 1
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    def dar_like(self):
-        self.__likes += 1
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    def dar_like(self):
-        self.__likes += 1
 
 
 
