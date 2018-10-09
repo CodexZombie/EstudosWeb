b48c83cdef74e4f7194ecc54bf9db4225fb8f634
CodexZombie <walvesdg@gmail.com>
2018-10-08 21:17:38 -0300

    Tela de login e secret key

--- carreira-python/flask/jogoteca/jogoteca.py
+++ carreira-python/flask/jogoteca/jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'jogoteca'
 
 class Jogo:
     def __init__(self, nome, categoria, plataforma):
@@ -50,6 +51,29 @@ def criar():
     jogo = Jogo(nome, categoria, plataforma)
     lista.append(jogo)
 
+    return redirect('/')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso!')
+        return redirect('/')
+    else:
+        flash('Não logado, tente novamente.')
+        return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
     return redirect('/')
 
 
