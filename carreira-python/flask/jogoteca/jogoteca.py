a5449edaf8a8b7877f1dc9b2279a37e0befa51d4
CodexZombie <walvesdg@gmail.com>
2018-10-08 22:44:11 -0300

    Proteção das rotas e fluxo de login

--- carreira-python/flask/jogoteca/jogoteca.py
+++ carreira-python/flask/jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = 'jogoteca'
@@ -22,6 +22,34 @@ class Jogo:
         return self.__plataforma
 
 
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.__id = id
+        self.__nome = nome
+        self.__senha = senha
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def senha(self):
+        return self.__senha
+
+
+usuario1 = Usuario('luan', 'Luiz Antonio Marques', '1234')
+usuario2 = Usuario('Nico', 'Nico Steppat', '7a1')
+usuario3 = Usuario('flavio', 'flavio Almeida', 'javascript')
+
+usuarios = {usuario1.id: usuario1,
+            usuario2.id: usuario2,
+            usuario3.id: usuario3}
+
+
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('Super Mario', 'Aventura', 'Super Nintendo')
 jogo3 = Jogo('Pokemon Silver', 'RPG', 'Game Boy')
@@ -31,18 +59,17 @@ lista = [jogo1, jogo2, jogo3]
 
 @app.route('/')
 def index():
-    return render_template('lista.html',
-                           titulo='Jogos',
-                           jogos=lista)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html',
-                           titulo='Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo Jogo')
 
 
-@app.route('/criar', methods=['POST',])
+@app.route('/criar', methods=['POST', ])
 def criar():
     nome = request. form['nome']
     categoria = request. form['categoria']
@@ -51,30 +78,34 @@ def criar():
     jogo = Jogo(nome, categoria, plataforma)
     lista.append(jogo)
 
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if 'mestra' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/')
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
-        flash('Não logado, tente novamente.')
-        return redirect('/login')
+        flash('Não logado, tente novamente!')
+        return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 app.run(debug=True)
