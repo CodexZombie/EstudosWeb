7698c4dd7c058834c48f7002a77b4dd04513718b
CodexZombie <walvesdg@gmail.com>
2018-10-15 21:11:37 -0300

    Carregando arquivos para o servidor

--- carreira-python/flask/jogoteca/jogoteca.py
+++ carreira-python/flask/jogoteca/jogoteca.py
@@ -1,8 +1,9 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
 from flask_mysqldb import MySQL
 from dao import JogoDao, UsuarioDao
 from models import Jogo, Usuario
+import os
 
 app = Flask(__name__)
 app.secret_key = 'jogoteca'
@@ -13,6 +14,7 @@ app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "jogoteca"
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_CHARSET'] = 'utf8'
+app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 
 db = MySQL(app)
 jogo_dao = JogoDao(db)
@@ -32,15 +34,21 @@ def novo():
     return render_template('novo.html', titulo='Novo Jogo')
 
 
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
+
+
 @app.route('/criar', methods=['POST', ])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-
     jogo = Jogo(nome, categoria, console)
     jogo_dao.salvar(jogo)
-
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{jogo.id}.jpg')
     return redirect(url_for('index'))
 
 
@@ -49,7 +57,7 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = jogo_dao.busca_por_id(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=f'capa{id}.jpg')
 
 
 @app.route('/atualizar', methods=['POST', ])
