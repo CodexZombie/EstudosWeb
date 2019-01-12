6b82bf7599e37c46e8152cfa1e442245873f0470
CodexZombie <walvesdg@gmail.com>
2019-01-12 21:38:15 -0200

    add tratativa de erro

--- front/alura/javascript/js/buscar-pacientes.js
+++ front/alura/javascript/js/buscar-pacientes.js
@@ -5,13 +5,22 @@ botaoAdicionar.addEventListener("click", function(){
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
 
-        var pacientes = JSON.parse(resposta);
-      
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel")
+        }
     });
 
     xhr.send();
