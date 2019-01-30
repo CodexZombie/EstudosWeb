fdcdecea7ed7ddf59d5a188f5cb5b89c3cbf0d9d
CodexZombie <walvesdg@gmail.com>
2019-01-30 21:10:38 -0200

    Adicionando listener à textarea

--- front/alura/jquery/public/js/main.js
+++ front/alura/jquery/public/js/main.js
@@ -5,5 +5,17 @@ var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
 
-console.log(numPalavras);
+var campo = $(".campo-digitacao");
+
+campo.on("input", function(){
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+
 
